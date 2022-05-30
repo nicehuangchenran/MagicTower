6ba683015ceab9e87d321801890530c012e6baa3
焦骜 <2050780@tongji.edu.cn>
2022-05-30 20:28:52 +0800

    update from jiaoao 5.30

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -22,9 +22,8 @@
  THE SOFTWARE.
  ****************************************************************************/
 
-#include "HelloWorldScene.h"
-#include "SimpleAudioEngine.h"
 
+#include "HelloWorldScene.h"
 USING_NS_CC;
 
 Scene* HelloWorld::createScene()
@@ -42,92 +41,58 @@ static void problemLoading(const char* filename)
 // on "init" you need to initialize your instance
 bool HelloWorld::init()
 {
-    //////////////////////////////
-    // 1. super init first
+    
     if ( !Scene::init() )
     {
         return false;
     }
 
-    auto visibleSize = Director::getInstance()->getVisibleSize();
-    Vec2 origin = Director::getInstance()->getVisibleOrigin();
+    auto visibleSize = Director::getInstance()->getVisibleSize();  // 定义地图的可视化尺寸
+    Vec2 origin = Director::getInstance()->getVisibleOrigin();  // 定义地图的可视化原点
+    auto backgroudImage = Sprite::create("魔塔.png", Rect(240,0,550,483));
+    backgroudImage -> setAnchorPoint(Vec2::ZERO);
+    this -> addChild(backgroudImage);
+    
 
-    /////////////////////////////
-    // 2. add a menu item with "X" image, which is clicked to quit the program
-    //    you may modify it.
-
-    // add a "close" icon to exit the progress. it's an autorelease object
-    auto closeItem = MenuItemImage::create(
-                                           "CloseNormal.png",
-                                           "CloseSelected.png",
-                                           CC_CALLBACK_1(HelloWorld::menuCloseCallback, this));
-
-    if (closeItem == nullptr ||
-        closeItem->getContentSize().width <= 0 ||
-        closeItem->getContentSize().height <= 0)
-    {
-        problemLoading("'CloseNormal.png' and 'CloseSelected.png'");
-    }
-    else
-    {
-        float x = origin.x + visibleSize.width - closeItem->getContentSize().width/2;
-        float y = origin.y + closeItem->getContentSize().height/2;
-        closeItem->setPosition(Vec2(x,y));
-    }
-
-    // create menu, it's an autorelease object
-    auto menu = Menu::create(closeItem, NULL);
-    menu->setPosition(Vec2::ZERO);
-    this->addChild(menu, 1);
-
-    /////////////////////////////
-    // 3. add your codes below...
-
-    // add a label shows "Hello World"
-    // create and initialize a label
-
-    auto label = Label::createWithTTF("Hello World", "fonts/Marker Felt.ttf", 24);
+    auto label = Label::createWithTTF("魔塔50层", "fonts/Songti.ttc", 40);
+    auto magic_tower = Label::createWithTTF("Magic Tower", "fonts/Apple Chancery.ttf", 30);
     if (label == nullptr)
     {
-        problemLoading("'fonts/Marker Felt.ttf'");
+        problemLoading("fonts/Songti.ttc");
+    }
+    if (magic_tower == nullptr)
+    {
+        problemLoading("fonts/Apple Chancery.ttf");
     }
     else
     {
         // position the label on the center of the screen
         label->setPosition(Vec2(origin.x + visibleSize.width/2,
-                                origin.y + visibleSize.height - label->getContentSize().height));
-
+                                origin.y + visibleSize.height - label->getContentSize().height * 3));
+        magic_tower -> setPosition(Vec2(origin.x + visibleSize.width/2,
+                                        origin.y + visibleSize.height - label->getContentSize().height * 4));
         // add the label as a child to this layer
+        this -> addChild(magic_tower, 1);
         this->addChild(label, 1);
     }
+    
+    MenuItemFont::setFontName("fonts/arial.ttf");
+    MenuItemFont::setFontSize(28);
+    auto item1 = MenuItemFont::create("重新开始游戏");  // 文本菜单，接受两个参数，要显示的文本和回调函数指针
+    auto item2 = MenuItemFont::create("载入存档");
+    auto item3 = MenuItemFont::create("游戏说明");
+    auto item4 = MenuItemFont::create("选项");
+    auto item5 = MenuItemFont::create("退出游戏");
 
-    // add "HelloWorld" splash screen"
-    auto sprite = Sprite::create("HelloWorld.png");
-    if (sprite == nullptr)
-    {
-        problemLoading("'HelloWorld.png'");
-    }
-    else
-    {
-        // position the sprite on the center of the screen
-        sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
-
-        // add the sprite as a child to this layer
-        this->addChild(sprite, 0);
-    }
+    
+    // create menu, it's an autorelease object
+    auto menu = Menu::create(item1, item2, item3, item4, item5, NULL);
+    menu -> alignItemsVertically();
+    menu->setPosition(Point(origin.x + visibleSize.width/2, origin.y + visibleSize.height - label->getContentSize().height * 7));
+    this->addChild(menu, 1);
+     
     return true;
 }
 
 
-void HelloWorld::menuCloseCallback(Ref* pSender)
-{
-    //Close the cocos2d-x game scene and quit the application
-    Director::getInstance()->end();
 
-    /*To navigate back to native iOS screen(if present) without quitting the application  ,do not use Director::getInstance()->end() as given above,instead trigger a custom event created in RootViewController.mm as below*/
-
-    //EventCustom customEndEvent("game_scene_close_event");
-    //_eventDispatcher->dispatchEvent(&customEndEvent);
-
-
-}
