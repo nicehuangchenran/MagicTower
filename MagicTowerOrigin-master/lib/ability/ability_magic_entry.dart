99847e145864c7417e75d95a92851122255b3317
Jiao Ao <2050780@tongji.edu.cn>
2022-05-30 14:52:07 +0800

    Delete MagicTowerOrigin-master directory

--- MagicTowerOrigin-master/lib/ability/ability_magic_entry.dart
+++
@@ -1,5 +0,0 @@
-import 'package:magic_tower_origin/ability/ability_entry.dart';
-
-/// 魔术士 基础能力
-class AbilityMagicEntry extends AbilityEntry {
-}
--- MagicTowerOrigin-master/lib/map/map_info.dart
+++
@@ -1,7 +0,0 @@
-class MapInfo {
-  static var curLevel;
-  static var levelCount;
-
-  static var sx = 13;
-  static var sy = 13;
-}
