604f6e41aa6ff5fd8b8ddefba06897fc84cd74cc
Cu_OH_2 <104898033+Cu-OH-2@users.noreply.github.com>
2022-05-30 15:45:30 +0800

    Delete MagicTowerOrigin-master directory

--- MagicTowerOrigin-master/lib/ability/prop_type.dart
+++
@@ -1,7 +0,0 @@
-/// 道具类别，功能
-class PropType {
-  static const int ABILITY_BOOK = 1;
-  static const int ABILITY_SYMMETRY = 2;
-  static const int ABILITY_FLY_UP = 3;
-  static const int ABILITY_FLY_DOWN = 4;
-}
